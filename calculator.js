d7cb4d8b6e3676c8f9b40d934e0633c676bf67eb
rodrigosites <rodrigo.sites@gmail.com>
2023-01-31 16:25:35 -0300

    Better functionality to the calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 // DOM control
 let userClick = '';
-let clickedValue = '';
+let clickedNumber = '';
 let clickedOperator = '';
 const numberStack = [];
 const operatorStack = [];
@@ -25,31 +25,29 @@ operatorButtons.forEach(button => button.addEventListener('click', (event) => {
     case 'Del': 
       delChar();
     break;
-    case '=':
-      if (numberStack.length === 1 && userClick !== '') {
-        numberStack.push(userClick);
-        displayPartialText.textContent = `${numberStack[0]} ${operatorStack[0]} ${userClick} =`;
-        operateValues(operatorStack[0]);
-        clearValues();
-      } else if (numberStack.length === 1 && userClick === '') {
-          operatorStack.push(operatorStack[0]);
-          if (clickedValue !== '') {
-            numberStack.push(clickedValue);
-          } else {
-            numberStack.push(numberStack[0]);
-            clickedValue = numberStack[1];
-          }
-          displayPartialText.textContent = `${numberStack[0]} ${operatorStack[0]} ${clickedValue} =`;
-          operateValues(operatorStack[0]);
-      } else {
-        displayPartialText.textContent = '0 =';
-        clearValues();
-      }
-    break;
     default:
-      storeValue(clickedOperator);
+      if (userClick != '') {
+        clickedNumber = userClick;
+        userClick = '';
+        numberStack.push(clickedNumber);
+        operatorStack.push(clickedOperator)
+      } else {
+        if (numberStack.length === 0) {
+          numberStack.push(0);
+          operatorStack.push(clickedOperator);
+        } else {
+          if (clickedOperator === '=' && operatorStack[0] !== '=') {
+            operatorStack.push(clickedOperator)
+          } else {
+          operatorStack[0] = clickedOperator;
+          clickedNumber = numberStack[0];
+          }
+        }
+      }
+      displayPartialText.textContent = `${numberStack[0]} ${clickedOperator}`;
+      operateValues(operatorStack[0]);
     break;
-    }
+  }
 }));
 
 const clearDisplay = () => {
@@ -60,9 +58,10 @@ const clearDisplay = () => {
 
 const clearValues = () => {
   userClick = '';
-  clickedValue = '';
+  clickedNumber = '';
   clickedOperator = '';
   numberStack.length = 0; // clear the array without loop, neat!
+  operatorStack.length = 0; // clear the array without loop, neat!
 }
 
 const delChar = () => {
@@ -70,25 +69,20 @@ const delChar = () => {
   displayMainText.textContent = userClick;
 }
 
-const storeValue = (operator) => {
-  if (userClick !== '') numberStack.push(userClick);
-  if (numberStack.length > 1) operateValues(operatorStack[0]);
-  if (operatorStack.length === 0) {
-    operatorStack.push(operator)
-  } else {
-    operatorStack[0] = operator;
-    clickedValue = numberStack[0];
-  }
-  displayPartialText.textContent = `${numberStack[0]} ${operatorStack[0]}`;
-  userClick = '';
-}
-
 const operateValues = (operator) => {
-  numberStack[0] = operate(operator, parseInt(numberStack[0]), parseInt(numberStack[1]));
-  numberStack.pop();
-  operatorStack.shift();
+  if (numberStack.length === 2) {
+    if (operatorStack[1] === '=') displayPartialText.textContent = `${numberStack[0]} ${operatorStack[0]} ${numberStack[1]} =`;
+    numberStack[0] = operate(Number(numberStack[0]), operator, Number(numberStack[1]));
+    if (operatorStack[1] !== '=') displayPartialText.textContent = `${numberStack[0]} ${operatorStack[0]}`;
+    numberStack.pop();
+    operatorStack.shift();
+    if (operatorStack[0] === '=') operatorStack.pop();
+  } else if (operatorStack.length > 1) {
+    displayPartialText.textContent = `${numberStack[0]} ${operatorStack[0]} ${clickedNumber} =`
+    numberStack[0] = operate(Number(numberStack[0]), operatorStack[0], Number(clickedNumber));
+    operatorStack.pop();
+  }
   displayMainText.textContent = `${numberStack[0]}`;
-  userClick = '';
 }
 
 // mathematic logic
@@ -98,11 +92,12 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
-const operate = (operator,a,b) => {
+const operate = (a,operator,b) => {
   switch (operator) {
     case '+': return add(a,b);
     case '-': return subtract(a,b);
     case 'x': return multiply(a,b);
-    case '/': return divide(a,b);
+    case '/': return b === 0 ? 'You can\'t divide a number by 0' : divide(a,b);
+    break;
   }
 };
